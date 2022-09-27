31a02285403c61f7cce01e9990ade2c2d75ea83c
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-27 02:25:04 -0400

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# The following variables must be set before running this script.
+#
+# SUBSCRIPTION_ID: Azure Subscription ID
+# LOCATION: Azure Region for deployment
+# RESOURCE_GROUP: Resourece Group Name
+SUBSCRIPTION_ID=$1
+LOCATION=$2
+RESOURCE_GROUP=$3
+
+echo '##[section]Horde Storage - Deploy Bicep Template'
+az bicep install --version v0.10.61
+
+az group create \
+    --name "$RESOURCE_GROUP"\
+    --location "$LOCATION" \
+    --subscription "$SUBSCRIPTION_ID"
+
+az deployment group create \
+    --name "$RESOURCE_GROUP" \
+    --subscription "$SUBSCRIPTION_ID" \
+    --resource-group "$RESOURCE_GROUP" \
+    --template-file main.bicep \
+    --parameters confs/studio.parameters.json \
+    || exit 1
