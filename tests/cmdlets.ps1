a5d9785f3ca071927036b18d7c7f2e3e1a9be6f2
Daniel Ciborowski <dciborow@microsoft.com>
2022-10-27 18:35:05 -0400

    Update cmdlets.ps1

--- tests/cmdlets.ps1
+++ tests/cmdlets.ps1
@@ -1,7 +1,4 @@
 function Upload-BenchmarkECR {
-    Write-Host "Logging in to AWS ECR"
-    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 728559092788.dkr.ecr.us-east-1.amazonaws.com
-
     Write-Host "Starting docker build"
     docker build -t benchmarker .
 }
