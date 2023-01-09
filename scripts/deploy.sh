459d52b34f91f09bce47f242713ec6b976694127
Daniel Ciborowski <dciborow@microsoft.com>
2023-01-09 15:02:06 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@
 PARAMETERS_FILE=$1
 
 LOCATION=$(jq -r '.parameters.location.value' "$PARAMETERS_FILE")
-RESOURCE_GROUP=$(jq -r '.metadata' "$PARAMETERS_FILE")
+RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' "$PARAMETERS_FILE")
 
 echo '##[section]Horde Storage - Deploy Bicep Template'
 az bicep install --version v0.11.1
